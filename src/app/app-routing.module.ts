a2972ebbbe57a0f1b42048171bee3da415bdf021
hamza hejja <hamza.hejja@dimensions-healthcare.net>
2018-07-31 11:57:17 +0300

    Main Page and Login Page Added to Application

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+const routes: Routes = [
+  {
+    path: 'root',
+    component: MainPageComponent
+  },
+  {
+      path: 'login',
+      component: LoginPageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
